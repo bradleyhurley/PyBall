0169a9d765bc69046b6379be7c76d83c9e52af29
Austin Hacker <aehacker@us.ibm.com>
2018-03-05 21:42:07 -0500

    Adding error handling and basic integration tests

--- PyBall.py
+++ PyBall.py
@@ -5,25 +5,33 @@ from models.people import People
 from models.game_types import GameType
 from models.venue import Venue
 
+from error_parser import ErrorParser
+
 
 class PyBall:
     def __init__(self):
         self.r = requests
 
+    def _get(self, url):
+        results = self.r.get(url).json()
+        if 'messageNumber' in results:
+            ErrorParser.parse_error(results)
+        return results
+
     def get_player(self, player_id):
         url = "{0}/people/{1}".format(BASE_URL, player_id)
-        results = self.r.get(url).json()
+        results = self._get(url)
         return People(**results['people'][0])
 
     def get_game_types(self):
         game_types = []
         url = "{0}/gameTypes".format(BASE_URL)
-        results = self.r.get(url).json()
+        results = self._get(url)
         for game_type in results:
             game_types.append(GameType(**game_type))
         return game_types
 
     def get_venue(self, venue_id):
         url = "{0}/venues/{1}".format(BASE_URL, venue_id)
-        results = self.r.get(url).json()
+        results = self._get(url)
         return Venue(**results['venues'][0])
---
+++ test/test_player.py
@@ -0,0 +1,27 @@
+import pytest
+from PyBall import PyBall
+from models.people import People
+
+from exceptions import *
+
+
+@pytest.fixture(scope='module')
+def test_player():
+    pyball = PyBall()
+    return pyball.get_player(446372)
+
+
+def test_get_player_endpoint_returns_player(test_player):
+    assert isinstance(test_player, People)
+
+
+def test_bad_player_id():
+    pyball = PyBall()
+    with pytest.raises(InvalidIdError):
+        pyball.get_player(-1)
+
+
+def test_player_id_not_a_num():
+    pyball = PyBall()
+    with pytest.raises(BadRequestError):
+        pyball.get_player("not_an_id")
