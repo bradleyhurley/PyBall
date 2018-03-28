798f684d273c388096b128ee4f09889bc3bc2f4e
Brad Hurley <bradley.hurley@gmail.com>
2018-03-27 21:23:33 -0400

    Adding Game Status

--- PyBall/PyBall.py
+++ PyBall/PyBall.py
@@ -18,6 +18,7 @@ from PyBall.models.config.schedule_event_type import ScheduleEventType
 from PyBall.models.config.situation_code import SituationCode
 from PyBall.models.config.stats_group import StatsGroup
 from PyBall.models.config import StatsType
+from PyBall.models.config import GameStatus
 
 from PyBall.models.divisions import Division
 
@@ -52,7 +53,9 @@ class PyBall:
         return Venue(**results['venues'][0])
 
     def get_game_status(self):
-        pass
+        url = "{0}gameStatus".format(BASE_URL)
+        results = self._get(url)
+        return [GameStatus(**game_status) for game_status in results]
 
     def get_game_types(self):
         url = "{0}/gameTypes".format(BASE_URL)
---
+++ PyBall/models/config/game_status.py
@@ -0,0 +1,12 @@
+from PyBall.models import BaseModel
+
+
+class GameStatus(BaseModel):
+    _fields = {
+        'abstractGameState': {'default_value': None, 'field_type': str},
+        'codedGameState': {'default_value': None, 'field_type': str},
+        'detailedState': {'default_value': None, 'field_type': str},
+        'statusCode': {'default_value': None, 'field_type': int},
+        'reason': {'default_value': None, 'field_type': int},
+        'abstractGameCode': {'default_value': None, 'field_type': int},
+    }
