c527649b51f1b9544c8d31f400031ce4e960a3ef
a-hacker <explosivepyrounicorns@gmail.com>
2018-07-23 23:11:03 -0400

    Convert conference models

--- pyball/models/conference/conference.py
+++ pyball/models/conference/conference.py
@@ -1,16 +1,22 @@
-from pyball.models.base_model import BaseModel
+from dataclasses import dataclass, field
 from .sport import Sport
 from .league import League
 
 
-class Conference(BaseModel):
-    _fields = {
-        'id': {'default_value': None, 'field_type': int},
-        'link': {'default_value': None, 'field_type': str},
-        'name': {'default_value': None, 'field_type': str},
-        'abbreviation': {'default_value': None, 'field_type': str},
-        'hasWildcard': {'default_value': None, 'field_type': bool},
-        'nameShort': {'default_value': None, 'field_type': str},
-        'league': {'default_value': {}, 'field_type': League},
-        'sport': {'default_value': {}, 'field_type': Sport},
-    }
+@dataclass
+class Conference:
+    id: int = field(default=None)
+    link: str = field(default=None)
+    name: str = field(default=None)
+    abbreviation: str = field(default=None)
+    hasWildcard: bool = field(default=None)
+    nameShort: str = field(default=None)
+    league: League = field(default=None)
+    sport: Sport = field(default=None)
+
+    def __post_init__(self):
+        if isinstance(self.league, dict):
+            self.league = League(**self.league)
+
+        if isinstance(self.sport, dict):
+            self.sport = Sport(**self.sport)
